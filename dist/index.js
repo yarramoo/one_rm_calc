8084105b780a1d386aac0bd254d3c948125422a3
Mitchell Moore <u6666820@anu.edu.au>
2023-12-07 21:42:11 +1100

    Started adding some content

--- dist/index.js
+++ dist/index.js
@@ -11,21 +11,36 @@ import init, { find_onerm } from '../pkg/one_rm_calc.js';
 function initCalculateButton() {
     let buttonElem = document.getElementById('calculateButton');
     let repsInputElem = document.getElementById('repsInput');
-    let weightInputElem = document.getElementById('wieghtInput');
+    let weightInputElem = document.getElementById('weightInput');
     let resultElem = document.getElementById('result');
     buttonElem.addEventListener('click', function () {
         let reps = parseInt(repsInputElem.value);
         let weight = parseFloat(weightInputElem.value);
         var resultStr = "";
-        // Do some error handling on the inputted values
-        // Reps too low
-        if (reps == 0) {
+        // Check that the values inputted parsed correctly
+        if (Number.isNaN(reps) && Number.isNaN(weight)) {
+            resultStr = "Wtf have you given me??";
         }
+        else if (Number.isNaN(reps)) {
+            resultStr = "You've done a wild number of reps. For real, that \"number\" is wild. I don't believe that is a number";
+        }
+        else if (Number.isNaN(weight)) {
+            resultStr = "Unearthly weight my dude. Certainly unparsable. Try again.";
+        }
+        if (resultStr != "") {
+            resultElem.innerHTML = resultStr;
+            return;
+        }
+        // Calculate the one RM 
         let one_rm = find_onerm(reps, weight);
         if (one_rm === undefined) {
             resultStr = "What the heck did you give me??";
         }
+        else {
+            resultStr = one_rm.toFixed(2);
+        }
         resultElem.innerHTML = resultStr;
+        console.log("Updated button");
     });
     console.log("Initted button...");
 }
@@ -35,7 +50,6 @@ function init_elements() {
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         let wasm = yield init();
-        alert("HERE!");
         init_elements();
     });
 }
--- ts_src/index.ts
+++ ts_src/index.ts
@@ -3,27 +3,37 @@ import init, { find_onerm } from '../pkg/one_rm_calc.js'
 function initCalculateButton() {
     let buttonElem = document.getElementById('calculateButton') as HTMLButtonElement;
     let repsInputElem = document.getElementById('repsInput') as HTMLInputElement;
-    let weightInputElem = document.getElementById('wieghtInput') as HTMLInputElement;
+    let weightInputElem = document.getElementById('weightInput') as HTMLInputElement;
     let resultElem = document.getElementById('result') as HTMLParagraphElement;
 
     buttonElem.addEventListener('click', function() {
         let reps = parseInt(repsInputElem.value);
         let weight = parseFloat(weightInputElem.value);
         var resultStr = "";
-    
-        // Do some error handling on the inputted values
-        // Reps too low
-        if (reps == 0) {
-    
-        }
-    
-        let one_rm = find_onerm(reps, weight);
 
+        // Check that the values inputted parsed correctly
+        if (Number.isNaN(reps) && Number.isNaN(weight)) {
+            resultStr = "Wtf have you given me??";
+        } else if (Number.isNaN(reps)) {
+            resultStr = "You've done a wild number of reps. For real, that \"number\" is wild. I don't believe that is a number";
+        } else if (Number.isNaN(weight)) {
+            resultStr = "Unearthly weight my dude. Certainly unparsable. Try again."
+        }
+        if (resultStr != "") {
+            resultElem.innerHTML = resultStr;
+            return;
+        }
+        // Calculate the one RM 
+        let one_rm = find_onerm(reps, weight);
+        
         if (one_rm === undefined) {
             resultStr = "What the heck did you give me??";
+        }else {
+            resultStr = one_rm.toFixed(2);
         }
     
         resultElem.innerHTML = resultStr;
+        console.log("Updated button");
     });
 
     console.log("Initted button...");
@@ -36,7 +46,6 @@ function init_elements() {
 async function run() {
     let wasm = await init();
 
-    alert("HERE!");
 
     init_elements();
 }
